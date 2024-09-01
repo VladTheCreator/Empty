8a88873cb5729004a0ae4abd8ff4aca876ad5c16
Vlad <vladys.kuleev@gmail.com>
2024-09-01 10:55:37 +0200

    Revert "Merge branch 'Reduce-health-bar-value-on-enemy-touch'"
    This reverts commit 5a4fc2155e1ef9aa5da1397b07e5701d3756680d, reversing
    changes made to 4d754d8e4f689bfdeb36a17edb2cf3c88a87467f.

--- EmptyProject/Assets/Scripts/EnemyTouch.cs
+++ EmptyProject/Assets/Scripts/EnemyTouch.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class EnemyTouch : MonoBehaviour
 {
     [SerializeField] GameObject Enemy;
-    [SerializeField] Health health;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            health.DealDamage(10);
             Destroy(collision.gameObject);
         }
     }
